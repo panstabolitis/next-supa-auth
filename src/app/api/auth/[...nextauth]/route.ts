29b2b926daa108d7061a58679392d82aa41fab02
panstabolitis <stabol.panay@gmail.com>
2023-09-25 16:57:40 +0300

    implemented jtw callback

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -17,7 +17,23 @@ export const authOptions: NextAuthOptions = {
     adapter: SupabaseAdapter({
         url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
         secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? ""
-    }) as Adapter
+    }) as Adapter,
+    callbacks: {
+        async session({ session, user }) {
+          const signingSecret = process.env.SUPABASE_JWT_SECRET
+          if (signingSecret) {
+            const payload = {
+              aud: "authenticated",
+              exp: Math.floor(new Date(session.expires).getTime() / 1000),
+              sub: user.id,
+              email: user.email,
+              role: "authenticated",
+            }
+            session.supabaseAccessToken = jwt.sign(payload, signingSecret)
+          }
+          return session
+        },
+    }
 };
 
 
--- src/lib/useClient.ts
+++ src/lib/useClient.ts
@@ -1,12 +1,32 @@
+import { createClient } from '@supabase/supabase-js'; // Import this if not already imported
+import { useSession } from 'next-auth/react';
 
-import { createClient } from '@supabase/supabase-js'
+// Use `useSession()` or `unstable_getServerSession()` to get the NextAuth session.
+const { data: session } = useSession() || {};
+const { supabaseAccessToken } = session || {}; // Destructure supabaseAccessToken and use an empty object as a fallback if session is undefined.
 
-export const supabase = createClient(
+const createSupabaseClient = () => {
+  // Check if supabaseAccessToken is null or empty
+  if (!supabaseAccessToken) {
+    // Return client without the "global"
+    return createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    );
+  }
+  
+  // Return client with the "global"
+  return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-        db: { 
-            schema: "next_auth"
-        }
+      global: {
+        headers: {
+          Authorization: `Bearer ${supabaseAccessToken}`,
+        },
+      },
     }
-);
+  );
+}
+
+export const supabase = createSupabaseClient();
